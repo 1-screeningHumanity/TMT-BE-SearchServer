f3ceab7e485bdff25aa2799b9926da43edf05599
KangBaekGwa <ksu9801@gmail.com>
2024-05-29 14:37:40 +0900

    Feat : 신규 P/J Setting

---
+++ src/main/java/ScreeningHumanity/SearchServer/SearchServerApplication.java
@@ -0,0 +1,13 @@
+package ScreeningHumanity.SearchServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ScreeningHumanity/SearchServer/SearchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ScreeningHumanity.SearchServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
