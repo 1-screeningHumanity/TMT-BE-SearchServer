363f0bcea2fed3c99b75918ae6e6c879274d8f9d
KangBaekGwa <ksu9801@gmail.com>
2024-06-05 13:48:57 +0900

    Fix : Json 파싱 하여 반환 시, 순서 보장 보장되도록 설정.

--- src/main/java/ScreeningHumanity/SearchServer/application/port/out/outdto/MemberSearchOutDto.java
+++ src/main/java/ScreeningHumanity/SearchServer/application/port/out/outdto/MemberSearchOutDto.java
@@ -1,6 +1,7 @@
 package ScreeningHumanity.SearchServer.application.port.out.outdto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@JsonPropertyOrder({ "id", "name" })
 public class MemberSearchOutDto {
 
     private Long id;
